bf01094782d573debfabb0974fe75b64a602d9c8
Muhammad Afzal <afzal2000@yahoo.com>
2020-08-18 11:50:56 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -4,7 +4,6 @@ import numpy as np
 digit=input()
 digit2=digit.split(',')
 digit3 = np.array(digit2,dtype='float32')
-display(digit3.reshape(28,28,1))
 
 
 from tensorflow.keras.models import load_model
