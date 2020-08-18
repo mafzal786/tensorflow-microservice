11f9b411f5264c0cb10edcd9213c21b4b60a9200
Muhammad Afzal <afzal2000@yahoo.com>
2020-08-18 09:20:41 -0400

    Create training_jupyter_notebook.py

---
+++ training_jupyter_notebook.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+print('Tensorflow version:' + tf.__version__)
+
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+input_shape = [28, 28, 1]
+
+X_train = tf.reshape(X_train, [X_train.shape[0]] + input_shape)
+X_test = tf.reshape(X_test, [X_test.shape[0]] + input_shape)
+
+X_train = tf.cast(X_train, dtype=tf.float32)
+X_test = tf.cast(X_test, dtype=tf.float32)
+
+X_train /= 255
+X_test /= 255
+
+import matplotlib.pyplot as plt
+def display(img):
+    plt.imshow(img,cmap=plt.cm.binary)
+    
+image_index = 35
+print(y_train[image_index])
+display(X_train[image_index])
+plt.show()
+
+print(X_train.shape)
+print(X_test.shape)
+
+from sklearn.preprocessing import OneHotEncoder
+
+# Reshape so labels can be onehot encoded.
+y_train = tf.reshape(y_train, [-1, 1])
+y_test = tf.reshape(y_test, [-1, 1])
+
+# Set sparse to False in order to return an array.
+encoder = OneHotEncoder(sparse=False)
+
+y_train = tf.convert_to_tensor(encoder.fit_transform(y_train))
+y_test = tf.convert_to_tensor(encoder.fit_transform(y_test))
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+  tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+  
+  tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  tf.keras.layers.Dropout(0.25) ,
+    
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'), 
+  tf.keras.layers.Dropout(0.5) ,
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+
+epoch=10
+batchsize=32
+
+loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam()
+
+model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
+
+# Train our model!
+h = model.fit(x=X_train, y=y_train, epochs=epoch, validation_data=(X_test, y_test), batch_size=batchsize)
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+model.save('model.h5')
